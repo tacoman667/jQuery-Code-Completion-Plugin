1957a4a730ec4d7d209154f8260b3a24b58e895f
tacoman667 <tacoman667gmail.com>
2011-06-14 08:47:23 -0500

    Started converting to a full on model for code completion

--- jQuery.CodeComplete.js
+++ jQuery.CodeComplete.js
@@ -1,19 +1,33 @@
+
+/*
+	model = {
+		keywords: <Array of String values>,
+		dataTypes: <Array of key/value pairs> ex. [ { type: 'string', methods: [ 'tostring', 'toupper', 'tolower' ] }, { type: 'int', methods: [ 'tostring' ] } ],
+		members: <Array of key/value pairs of type String> ex. [ { name: 'firstname', type: 'string' } ]
+	};
+*/
+
+
 (function($) {
 	$.fn.codeComplete = function(options) {
 	
 		options = $.extend({
-			keywords: [ "if" , "else" , "orelse" , 'firstname' , 'firstname.tostring']
+			model: {
+				keywords: [ 'if', 'else', 'orelse', 'and', 'not', '<>', '=', '<', '>' ],
+				dataTypes: [ { type: 'string', methods: [ 'tostring', 'toupper', 'tolower' ] }, { type: 'int', methods: [ 'tostring' ] } ],
+				members: [ { name: 'firstname', type: 'string' } ]
+			}
 		}, options);
-		options.keywords.sort();
+		options.model.keywords.sort();
 		
-		this.data("keywords", options.keywords);
+		this.data("model", options.model);
 		
 		this.bind("keyup", function(e) {
 			log('----------------------------- Begin KeyUp Event Handler -----------------------------');
 			log('Key ANSII Code: ' + e.keyCode);
 			if (e.keyCode === 8) return this;
 			
-			return replaceAndHighlightText($(this), options.keywords);
+			return replaceAndHighlightText($(this), options.model);
 			
 			log('----------------------------- End KeyUp Event Handler -----------------------------');
 		});
@@ -31,13 +45,13 @@
 			// Period
 			if (e.keyCode === 190) {
 				log('Period was pressed');
-				return replaceAndHighlightText($(this), options.keywords);
+				return replaceAndHighlightText($(this), options.model);
 			}
 			
 			// Enter
 			if (e.keyCode === 13) {
 				log('Enter was pressed');
-				replaceAndHighlightText($(this), options.keywords);
+				replaceAndHighlightText($(this), options.model);
 				$(this).val($(this).val() + " ");
 			}
 		});
@@ -46,21 +60,21 @@
 	};
 })(jQuery);
 
-function replaceAndHighlightText(self, keywords) {
+function replaceAndHighlightText(self, model) {
 	var words = self.val().split(" ");
 	var lastWord = words[words.length - 1];
 	log('Last Word: ' + lastWord);
 	
 	if (lastWord.length === 0) return self;		// Doesn't evaluate keywords on a space
-	for (var i = 0; i < keywords.length; i++) {
+	for (var i = 0; i < model.keywords.length; i++) {
 		var pattern = new RegExp('^' + lastWord);
-		var isMatch = pattern.test(keywords[i]);
+		var isMatch = pattern.test(model.keywords[i]);
 		
 		if (isMatch) {
-			log('Keyword matched with: ' + keywords[i]);
+			log('Keyword matched with: ' + model.keywords[i]);
 			
 			var valueLessLastWord = self.val().substring(0, (self.val().length - lastWord.length));
-			self.val(valueLessLastWord + keywords[i]);
+			self.val(valueLessLastWord + model.keywords[i]);
 			
 			var start = valueLessLastWord.length + lastWord.length;														// Set start of the highlight
 			var end = self.val().length;																									// Set end of the highlight
