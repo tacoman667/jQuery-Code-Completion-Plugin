c5a585f52541660b0bd75d18ef9a89c55731c47d
tacoman667 <tacoman667gmail.com>
2011-06-15 08:18:19 -0500

    Ran js through jslint to eliminate as many errors as possible

--- jQuery.CodeComplete.js
+++ jQuery.CodeComplete.js
@@ -7,9 +7,106 @@
 	}
 */
 
+function selectText(element, start, end) {
+	"use strict";
+	if (element[0].createTextRange) {
+		var selRange = element[0].createTextRange();
+		selRange.collapse(true);
+		selRange.moveStart('character', start);
+		selRange.moveEnd('character', end);
+		selRange.select();
+	} else if (element[0].setSelectionRange) {
+		element[0].setSelectionRange(start, end);
+	} else if (element[0].selectionStart) {
+		element[0].selectionStart = start;
+		element[0].selectionEnd = end;
+	}
+	element.focus();
+}
+
+function log(message) {
+	"use strict";
+	if (console) {
+		console.log(message);
+	}
+}
+
+Array.prototype.contains = function (value) {
+	for (var i in this) {
+		if (this[i] === value)
+			return true;
+	}
+	return false;
+}
+
+function replaceTextAndHighlight(self, lastWord, newWord) {
+	var valueLessLastWord = self.val().substring(0, (self.val().length - lastWord.length));
+	self.val(valueLessLastWord + newWord);
+	
+	var start = valueLessLastWord.length + lastWord.length;														// Set start of the highlight
+	var end = self.val().length;																									// Set end of the highlight
+	
+	selectText(self, start, end);																									// Set the selected text to the remainder of the letters from what was actually input
+	return self;
+}
+
+function evaluateMembers(self, lastWord, model) {
+	var members = [];
+	for (var i in model.members) {
+		members.push(model.members[i].name);
+	}
+	
+	for (var i in members) {
+		if (typeof members[i] === 'function') { continue; }
+		
+		var pattern = new RegExp('^' + lastWord);
+		var isMatch = pattern.test(members[i]);
+		
+		if (isMatch) {
+			log('Keyword matched with: ' + members[i]);
+			replaceTextAndHighlight(self, lastWord, members[i]);
+		}
+	}
+	
+	return self;
+}
+
+function evaluateKeywords(self, lastWord, keywords) {
+	for (var i in keywords) {
+		if (typeof keywords[i] === 'function') { continue; }
+		
+		var pattern = new RegExp('^' + lastWord);
+		var isMatch = pattern.test(keywords[i]);
+		
+		if (isMatch) {
+			log('Keyword matched with: ' + keywords[i]);
+			replaceTextAndHighlight(self, lastWord, keywords[i]);
+		}
+	}
+	
+	return self;
+}
+
+function executeCodeCompletion(self, model) {
+	var words = self.val().split(" ");
+	var lastWord = words[words.length - 1];
+	log('Last Word: ' + lastWord);
+	
+	if (lastWord.length === 0) { return self;	}																						// Doesn't evaluate keywords on a space
+	
+	// Evaluate model.keywords
+	evaluateKeywords(self, lastWord, model.keywords);
+	
+	// Evaluate model.members
+	evaluateMembers(self, lastWord, model);
+	
+	// Evaluate model.dataTypes
+	
+	return self;
+}
 
 (function($) {
-	$.fn.codeComplete = function(options) {
+	$.fn.codeComplete = function (options) {
 	
 		options = $.extend({
 			model: {
@@ -23,20 +120,17 @@
 		this.data("model", options.model);
 		
 		this.bind("keyup", function(e) {
-			log('----------------------------- Begin KeyUp Event Handler -----------------------------');
 			log('Key ANSII Code: ' + e.keyCode);
-			if (e.keyCode === 8) return this;
+			if (e.keyCode === 8) { return this; }
 			
 			return executeCodeCompletion($(this), options.model);
-			
-			log('----------------------------- End KeyUp Event Handler -----------------------------');
 		});
 		
 		this.bind('keydown', function(e) {
 			var self = $(this);
 			
 			// SpaceBar
-			if (e.keyCode == 32) {
+			if (e.keyCode === 32) {
 				log('Spacebar was pressed');
 				selectText(self, self.val().length, self.val().length);
 			}
@@ -59,98 +153,3 @@
 		return this;
 	};
 })(jQuery);
-
-function executeCodeCompletion(self, model) {
-	var words = self.val().split(" ");
-	var lastWord = words[words.length - 1];
-	log('Last Word: ' + lastWord);
-	
-	if (lastWord.length === 0) return self;																							// Doesn't evaluate keywords on a space
-	
-	// Evaluate model.keywords
-	evaluateKeywords(self, lastWord, model.keywords);
-	
-	// Evaluate model.members
-	evaluateMembers(self, lastWord, model);
-	
-	// Evaluate model.dataTypes
-	
-	return self;
-}
-
-function evaluateMembers(self, lastWord, model) {
-	var members = [];
-	for (var i in model.members) {
-		members.push(model.members[i].name);
-	}
-	
-	for (var i in members) {
-		if (typeof members[i] === 'function') continue;
-		
-		var pattern = new RegExp('^' + lastWord);
-		var isMatch = pattern.test(members[i]);
-		
-		if (isMatch) {
-			log('Keyword matched with: ' + members[i]);
-			replaceTextAndHighlight(self, lastWord, members[i]);
-		}
-	}
-	
-	return self;
-}
-
-function evaluateKeywords(self, lastWord, keywords) {
-	for (var i in keywords) {
-		if (typeof keywords[i] === 'function') continue;
-		
-		var pattern = new RegExp('^' + lastWord);
-		var isMatch = pattern.test(keywords[i]);
-		
-		if (isMatch) {
-			log('Keyword matched with: ' + keywords[i]);
-			replaceTextAndHighlight(self, lastWord, keywords[i]);
-		}
-	}
-	
-	return self;
-}
-
-function replaceTextAndHighlight(self, lastWord, newWord) {
-	var valueLessLastWord = self.val().substring(0, (self.val().length - lastWord.length));
-	self.val(valueLessLastWord + newWord);
-	
-	var start = valueLessLastWord.length + lastWord.length;														// Set start of the highlight
-	var end = self.val().length;																									// Set end of the highlight
-	
-	selectText(self, start, end);																									// Set the selected text to the remainder of the letters from what was actually input
-	return self;
-}
-
-function selectText(element, start, end) {
-	if( element[0].createTextRange ) {
-		var selRange = element[0].createTextRange();
-		selRange.collapse(true);
-		selRange.moveStart('character', start);
-		selRange.moveEnd('character', end);
-		selRange.select();
-	} else if( element[0].setSelectionRange ) {
-		element[0].setSelectionRange(start, end);
-	} else if(element[0].selectionStart ) {
-		element[0].selectionStart = start;
-		element[0].selectionEnd = end;
-	}
-	element.focus();
-}
-
-function log(message) {
-	if (console)
-		console.log(message);
-}
-
-Array.prototype.contains = function(value) {
-	for (var i in this) {
-		if (this[i] === value)
-			return true;
-	}
-	return false;
-}
